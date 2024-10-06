2877af5aa597bc4f57b85a10ec0b65c261c3dc5c
Tony Chu <tony.chu805@gmail.com>
2024-10-06 21:40:37 +0800

    model_trainer v2

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,7 @@ import dill
 
 from src.exception import CustomException
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 
 def save_object(file_path, obj):
@@ -27,7 +28,7 @@ def save_object(file_path, obj):
     
 
 
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
 
     try:
 
@@ -36,8 +37,15 @@ def evaluate_models(X_train, y_train, X_test, y_test, models):
         for i in range(len(list(models))):
 
             model = list(models.values())[i]
+            para = params[list(models.keys())[i]]
 
+            gs = GridSearchCV(model, para, cv=3)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train, y_train)
+
+            # model.fit(X_train, y_train)
 
             y_train_pred = model.predict(X_train)
 
